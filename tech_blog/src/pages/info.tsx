a4fe000a7ac7b522d0fc1230d3c0d42f9d7141c3
baesh3744 <baesh3744@naver.com>
2021-09-04 22:50:43 +0900

    feat: 03. 글 목록을 띄워 줄 메인 페이지 구현하기
    1. EmotionJS 라이브러리를 통한 컴포넌트 스타일링
    2. 메인 페이지의 기본적인 틀 만들기
    3. 카테고리 목록 컴포넌트 구현하기
    4. 게시글 목록 부분 컴포넌트 구현하기
    5. 메인 페이지 반응형 디자인 구현하기
        - max-width: 768px

--- tech_blog/src/pages/info.tsx
+++ tech_blog/src/pages/info.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import { graphql } from 'gatsby';
-import Text from 'components/Text';
-import { Link } from 'gatsby';
+import { Global, css } from '@emotion/react';
+import styled from '@emotion/styled';
 
 interface InfoPageProps {
     data: {
@@ -15,19 +15,49 @@ interface InfoPageProps {
     };
 }
 
+const globalStyle = css`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+
+        font-size: 20px;
+    }
+`;
+
+const TextStyle = css`
+    font-size: 18px;
+    font-weight: 700;
+    color: gray;
+`;
+
+// Kebab Case 적용
+const Text1 = styled.div<{ disable: boolean }>`
+    font-size: 20px;
+    font-weight: 700;
+    text-decoration: ${({ disable }) => (disable ? 'line-through' : 'none')};
+`;
+
+// Camel Case 적용
+const Text2 = styled('div')<{ disable: boolean }>(({ disable }) => ({
+    fontSize: '15px',
+    color: 'blue',
+    textDecoration: disable ? 'line-through' : 'none',
+}));
+
 const InfoPage: FunctionComponent<InfoPageProps> = function ({
     data: {
         site: {
-            siteMetadata: { title, description, author }
-        }
-    }
+            siteMetadata: { title, description, author },
+        },
+    },
 }) {
     return (
         <div>
-            <Text text={title} />
-            <Text text={description} />
-            <Text text={author} />
-            <Link to="/">To Main</Link>
+            <Global styles={globalStyle} />
+            <div css={TextStyle}>{title}</div>
+            <Text1 disable={true}>{description}</Text1>
+            <Text2 disable={true}>{author}</Text2>
         </div>
     );
 };
