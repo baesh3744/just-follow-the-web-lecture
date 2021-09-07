bc75083b30335a11e3de0f66145b4d3d77f853d4
baesh3744 <baesh3744@naver.com>
2021-09-07 17:40:10 +0900

    feat: 04. 메인 페이지에 실제 데이터 띄워보기
    1. Markdown 파일을 사용하기 위한 라이브러리 세팅하기
    2. Markdown 파일 추가 후, GraphQL Query 확인해보기
    3. 메인 페이지에서 Props로 받아 포스트 데이터 출력하기
    4. gatsby-image 라이브러리로 최적화된 썸네일 사진 띄워주기
    5. 카테고리 생성 및 카테고리별로 포스트 아이템 띄워주기
    6. 인피니티 스크롤 기능 구현하기

--- tech_blog/gatsby-config.js
+++ tech_blog/gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
                 allExtensions: true,
             },
         },
+        `gatsby-plugin-emotion`,
         `gatsby-plugin-react-helmet`,
         {
             resolve: `gatsby-source-filesystem`,
@@ -21,9 +22,53 @@ module.exports = {
                 path: `${__dirname}/contents`,
             },
         },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/static`,
+            },
+        },
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
-        `gatsby-plugin-emotion`,
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: 'gatsby-remark-smartypants',
+                        options: {
+                            dashes: 'oldschool',
+                        },
+                    },
+                    {
+                        resolve: 'gatsby-remark-prismjs',
+                        options: {
+                            classPrefix: 'language-',
+                        },
+                    },
+                    {
+                        resolve: 'gatsby-remark-images',
+                        options: {
+                            maxWidth: 768,
+                            quality: 100,
+                            withWebp: true
+                        },
+                    },
+                    {
+                        resolve: 'gatsby-remark-copy-linked-files',
+                        options: {},
+                    },
+                    {
+                        resolve: 'gatsby-remark-external-links',
+                        options: {
+                            target: '_blank',
+                            rel: 'nofollow',
+                        },
+                    },
+                ],
+            },
+        },
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,
