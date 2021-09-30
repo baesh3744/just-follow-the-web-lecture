7f144472e9363a91f00a4dffe6fad58b27b35265
baesh3744 <baesh3744@naver.com>
2021-09-30 17:23:48 +0900

    feat: #3 Nweeting
    #3 Nweeting
    #3.0 Form and Database Setup
    #3.1 Nweeting!
    #3.2 Getting the Nweets
    #3.3 Realtime Nweets
    #3.4 Delete and Update part One
    #3.5 Delete and Update part Two
    #3.6 Recap

---
+++ nwitter/src/components/Nweet.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { deleteDoc, doc, updateDoc } from "@firebase/firestore";
+import { dbService } from "fbase";
+
+const Nweet = ({ nweetObj, isOwner }) => {
+    const [editing, setEditing] = useState(false);
+    const [newNweet, setNewNweet] = useState(nweetObj.text);
+
+    const onDeleteClick = async () => {
+        const ok = window.confirm(
+            "Are you sure you want to delete this nweet?"
+        );
+        if (ok) {
+            await deleteDoc(doc(dbService, `nweets/${nweetObj.id}`));
+        }
+    };
+
+    const toggleEditing = () => setEditing((prev) => !prev);
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        updateDoc(doc(dbService, `nweets/${nweetObj.id}`), { text: newNweet });
+        setEditing(false);
+    };
+
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNewNweet(value);
+    };
+
+    return (
+        <div>
+            {editing ? (
+                <React.Fragment>
+                    {isOwner && (
+                        <React.Fragment>
+                            <form onSubmit={onSubmit}>
+                                <input
+                                    type='text'
+                                    placeholder='Edit your nweet'
+                                    value={newNweet}
+                                    onChange={onChange}
+                                    required
+                                />
+                                <input type='submit' value='Update Nweet' />
+                            </form>
+                            <button onClick={toggleEditing}>Cancel</button>
+                        </React.Fragment>
+                    )}
+                </React.Fragment>
+            ) : (
+                <React.Fragment>
+                    <h4>{nweetObj.text}</h4>
+                    {isOwner && (
+                        <React.Fragment>
+                            <button onClick={onDeleteClick}>
+                                Delete Nweet
+                            </button>
+                            <button onClick={toggleEditing}>Edit Nweet</button>
+                        </React.Fragment>
+                    )}
+                </React.Fragment>
+            )}
+        </div>
+    );
+};
+
+export default Nweet;
