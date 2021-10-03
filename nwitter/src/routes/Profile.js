51393f86f40be8df16cb89eecfc196a4ba17f172
baesh3744 <baesh3744@naver.com>
2021-10-03 14:40:32 +0900

    feat: #5 Edit Profile
    #5 Edit Profile
    #5.0 Get My Own Nweets
    #5.1 Update Profile
    #5.2 Update Profile Bufgix

--- nwitter/src/routes/Profile.js
+++ nwitter/src/routes/Profile.js
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router";
+import { updateProfile } from "@firebase/auth";
 import { authService } from "fbase";
 
-const Profile = () => {
+const Profile = ({ refreshUser, userObj }) => {
     let history = useHistory();
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
 
     const onLogOutClick = () => {
         authService.signOut();
         history.push("/");
     };
 
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        setNewDisplayName(value);
+    };
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        if (userObj.displayName !== newDisplayName) {
+            await updateProfile(userObj, { displayName: newDisplayName });
+        }
+        refreshUser();
+    };
+
     return (
         <React.Fragment>
+            <form onSubmit={onSubmit}>
+                <input
+                    type='text'
+                    placeholder='Display name'
+                    onChange={onChange}
+                    value={newDisplayName}
+                />
+                <input type='submit' value='Update Profile' />
+            </form>
             <button onClick={onLogOutClick}>Log Out</button>
         </React.Fragment>
     );
